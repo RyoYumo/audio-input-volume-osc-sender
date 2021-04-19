e2d6175357c09e56a16d73ddeb6a06d148b915ab
ryoyumo <yumo.after.fun@gmail.com>
2021-04-20 00:28:40 +0900

    fix 0.9 -> 0.11

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,10 +2,10 @@
 #include "ofxPubSubOsc.h"
 
 
-const int kBufferSize = 256;
-const int kSampleRate = 44100;
+constexpr auto kBufferSize = 256;
+constexpr auto kSampleRate = 44100;
 const std::string kAddress = "127.0.0.1";
-const int kPort = 8888;
+constexpr auto kPort = 8888;
 
 class ofApp : public ofBaseApp{
     
@@ -14,40 +14,46 @@ public:
     {
         ofBackground(0, 0, 0);
         ofSetWindowTitle("Audio Input Volume Sender App");
-        smoothed_vol = 0.f;
-        scaled_vol   = 0.f;
+        m_smoothed_vol = 0.f;
+        m_scaled_vol   = 0.f;
         setInputAudioDevice();
-        ofxPublishOsc(kAddress, kPort, "/volume", scaled_vol);
+        ofxPublishOsc(kAddress, kPort, "/volume", m_scaled_vol);
     }
     
     void setInputAudioDevice(){
-        auto device_list = sound.getDeviceList();
-        std::string device_name;
+        auto device_list = m_stream.getDeviceList();
+        std::stringstream ss;
         
         for(auto i = 0; i < device_list.size(); ++i){
             if(i == device_list.size()-1){
-                device_name += ofToString(device_list[i]);
+                ss << device_list[i];
             }else{
-                device_name += ofToString(device_list[i]) + "\n";
+                ss << device_list[i] << "\n";
             }
         }
         
-        int selected_device_id = ofToInt(ofSystemTextBoxDialog(device_name));
-        sound.setDeviceID(selected_device_id);
-        sound.setup(this, 0, 2, kSampleRate, kBufferSize, 4);
-        left.assign(kBufferSize, 0.f);
-        right.assign(kBufferSize, 0.f);
+        int selected_device_id = ofToInt(ofSystemTextBoxDialog(ss.str()));
+        ofSoundStreamSettings settings;
+        settings.setInDevice(device_list[selected_device_id]);
+        settings.setInListener(this);
+        settings.sampleRate = 44100;
+        settings.numOutputChannels = 0;
+        settings.numInputChannels = 2;
+        settings.bufferSize = kBufferSize;
+        m_stream.setup(settings);
+        m_left.assign(kBufferSize, 0.f);
+        m_right.assign(kBufferSize, 0.f);
     }
     
     void update(){
         
-        scaled_vol = ofClamp(ofMap(smoothed_vol, 0.f, 0.17, 0.f, 1.f), 0.f, 1.f);
+        m_scaled_vol = ofClamp(ofMap(m_smoothed_vol, 0.f, 0.17, 0.f, 1.f), 0.f, 1.f);
         //        scaled_vol = ofMap(smoothed_vol, 0.f, 0.17, 0.f, 1.f);
     }
     
     void draw(){
         ofSetColor(255,255,255);
-        ofDrawBitmapString("Volume: " + ofToString(scaled_vol), 20, 40);
+        ofDrawBitmapString("Volume: " + ofToString(m_scaled_vol), 20, 40);
         ofDrawBitmapString("IP: " + kAddress, 20, 60);
         ofDrawBitmapString("Port: " + ofToString(kPort), 20, 80);
         ofDrawBitmapString("---Key info ----", 20, 100);
@@ -59,29 +65,29 @@ public:
     
     void keyPressed(int key){
         if(key == 's'){
-            sound.stop();
+            m_stream.stop();
         }
         if(key == 'p'){
-            sound.start();
+            m_stream.start();
         }
     }
     
-    void audioIn(float* input, int bufferSize, int nChannels)
+    void audioIn(ofSoundBuffer& input)
     {
         float cur_vol = 0.f;
         int num_counted = 0;
         
-        for (auto i = 0 ; i < bufferSize; ++i){
-            left[i]  = input[i * 2] * 0.5;
-            right[i] = input[i * 2 + 1] * 0.5;
-            cur_vol += left[i] * left[i];
-            cur_vol += right[i] * right[i];
+        for (auto i = 0 ; i < input.getNumFrames(); ++i){
+            m_left[i]   = input[i * 2] * 0.5;
+            m_right[i]  = input[i * 2 + 1] * 0.5;
+            cur_vol     += m_left[i] * m_left[i];
+            cur_vol     += m_right[i] * m_right[i];
             num_counted += 2;
         }
         cur_vol /= static_cast<float>(num_counted);
         cur_vol = sqrt(cur_vol);
-        smoothed_vol *= 0.93;
-        smoothed_vol += 0.07 * cur_vol;
+        m_smoothed_vol *= 0.93;
+        m_smoothed_vol += 0.07 * cur_vol;
     }
     
     void drawAudioInput(){
@@ -92,17 +98,17 @@ public:
         ofDrawRectangle(0, 0, 240, 50);
         ofPopStyle();
         ofFill();
-        ofDrawRectangle(0,0 , 240 * scaled_vol, 50);
+        ofDrawRectangle(0,0 , 240 * m_scaled_vol, 50);
         ofPopMatrix();
     }
     
     
 private:
-    ofSoundStream sound;
-    vector<float> left;
-    vector<float> right;
-    float smoothed_vol;
-    float scaled_vol;
+    ofSoundStream m_stream;
+    vector<float> m_left;
+    vector<float> m_right;
+    float m_smoothed_vol;
+    float m_scaled_vol;
     
 };
 
